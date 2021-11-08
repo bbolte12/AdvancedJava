d2e6681e7a24074b9c0eae41011db1d6cdb4dad9
bbolte12 <blakebolte0@gmail.com>
2021-11-07 20:07:12 -0600

    cleaned up project

--- AdvJavaProj/src/Main.java
+++ AdvJavaProj/src/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException, SQLException {
 		
 		Scanner scanner = new Scanner(System.in);
-		int userInput = 0;
 		String username;
 		String password;
 		System.out.println("Please enter your username: ");
@@ -17,54 +16,6 @@ public class Main {
 		password = scanner.next();
 		
 		LogCheck.logCheck(username, password);
-		/*boolean name;
-		boolean key;
-		int userInput = 0;
-		Scanner scanner = new Scanner(System.in);
-		LogHandler.logHigh("started Program");//logs started program in logger
-		
-		System.out.println("Please enter username and password:\n");
-	
-		do {
-			// sets username
-			String userName = "Bob";
-			String nameAttempt = scanner.next();
-
-			name = ValidateUserInput.isValid(userName, nameAttempt);
-			// If wrong username, display message
-			if (!name) {
-				System.out.println("That username does not exist, please try again or create an account\n");
-				LogHandler.logLow("User incorrectly entered" + name);//logs incorrect login in logger, displays incorrect login used
-			}
-		} while (!name);
-		do {
-			// sets password
-			String password = "F!sh";
-			String passAttempt = scanner.next();
-
-			key = ValidateUserInput.isValid(password, passAttempt);
-			// If wrong password, display message
-			if (!key) {
-				System.out.println("Incorrect Password, please try again\n");
-			}
-		}
-		while (!key);
-		
-		//connect to database
-		try {
-			String urlConn = "jdbc:mysql://localhost:3306/advjavaproj";
-			@SuppressWarnings("unused")
-			Connection connection = DriverManager.getConnection(urlConn, "root", "Bb41907410@$");
-			System.out.println("Connection Successfull");	
-			LogHandler.logLow("Connection Successfull");//logs connection successful in logger
-		}
-		catch (Exception e) {
-			System.out.println("Error Connecting to the database: " + e.getMessage());
-			LogHandler.logLow("Connection Unsuccessfull");//logs connection unsuccessful in logger
-		}
-			
-		//displays menu for user selection
-		Menu.userSelect(userInput);*/
 		scanner.close();
 	}
 }
